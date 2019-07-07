6fb2b618f933262fa7bc46884e6305f0c3cf6812
Brieuc <brieuc.de-lantivy@epitech.eu>
2019-07-07 18:19:54 +0200

    [ADD] basic install

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+sudo dnf -y upgrade
+sudo dnf -y update
+sudo dnf -y install git
+sudo dnf -y install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sudo dnf -y install emacs
+
+#install vscode
+sudo dnf -y install code
+
+#install i3
+sudo dnf -y install i3 i3status dmenu i3lock xbacklight feh conky
+
+##
+## configure zsh
+##
+cd /tmp
+git clone https://github.com/Brieuclantivy/dotfiles
+cp .zshrc ~/
+cp .zsh_history ~/
+source ~/.zshrc
+
+
+
+#zsh custom plugins
+#zsh-autosuggestions  zsh-history-substring-search
+
+#i3
+#git
+#zsh
+#emacs
+#vscode
